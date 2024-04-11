7cd55dfc5068694ef67cd9133247c49e03b7a8ec
Avoyt <wolf.mail95@gmail.com>
2024-04-10 21:00:28 -0400

    Fixed default index page.

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -7,45 +7,40 @@
  * @package Arboreal_Theme
  */
 
+get_header();
 ?>
 
 <section class="no-results not-found">
 	<header class="page-header">
-		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'arboreal' ); ?></h1>
+		<h1 class="page-title"><?php esc_html_e('Nothing Found', 'arboreal'); ?></h1>
 	</header><!-- .page-header -->
 
-	<div class="page-content">
-		<?php
-		if ( is_home() && current_user_can( 'publish_posts' ) ) :
-
-			printf(
-				'<p>' . wp_kses(
-					/* translators: 1: link to WP admin new post page. */
-					__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'arboreal' ),
-					array(
-						'a' => array(
-							'href' => array(),
-						),
-					)
-				) . '</p>',
-				esc_url( admin_url( 'post-new.php' ) )
-			);
-
-		elseif ( is_search() ) :
-			?>
-
-			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'arboreal' ); ?></p>
-			<?php
-			get_search_form();
-
-		else :
-			?>
-
-			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'arboreal' ); ?></p>
-			<?php
-			get_search_form();
-
-		endif;
+	<?php
+	if (is_home() && current_user_can('publish_posts')):
 		?>
-	</div><!-- .page-content -->
+		<div class="page-content"> <?php
+		printf(
+			'<p>' . wp_kses(
+				/* translators: 1: link to WP admin new post page. */
+				__('Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'arboreal'),
+				array(
+					'a' => array(
+						'href' => array(),
+					),
+				)
+			) . '</p>',
+			esc_url(admin_url('post-new.php'))
+		);
+		?>
+		</div> <?php
+
+	elseif (is_search()):
+		?>
+		<div class="page-content">
+			<p><?php esc_html_e('Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'arboreal'); ?>
+			</p>
+			<?php get_search_form(); ?>
+		</div>
+	<?php endif; ?>
+	<!-- .page-content -->
 </section><!-- .no-results -->
