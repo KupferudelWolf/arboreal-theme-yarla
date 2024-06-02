a524f89aafdc6df43df957e5b985c0d5f2d64250
Avoyt <wolf.mail95@gmail.com>
2024-06-02 16:07:06 -0400

    Changed theme color to current header color.

--- template-parts/meta.php
+++ template-parts/meta.php
@@ -3,7 +3,7 @@
 <meta property="og:title" content="Arboreal | <?php echo get_the_title(); ?>">
 <meta property="og:description" content="Arboreal: A Comic From Another World">
 <meta property="og:image" content="<?php echo get_the_post_thumbnail_url() ?: get_the_post_thumbnail_url(); ?>">
-<meta name="theme-color" content="#FCFADE">
+<meta name="theme-color" content="#313f40">
 <meta name="twitter:card" content="summary_large_image">
 <meta name="twitter:title" content="Arboreal | <?php echo get_the_title(); ?>">
 <meta name="twitter:description" content="Arboreal: A Comic From Another World">
